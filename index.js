6357752d944f658e1353d7dc431d4e505453bf0d
Shehanka <chamodp@wso2.com>
2022-05-11 09:13:21 +0530

    Enable Disable Added

--- index.js
+++ index.js
@@ -1,41 +1,49 @@
 const core = require('@actions/core');
 const axios = require('axios').default;
 
-try {
-    const baseURL = core.getInput('baseURL');
-    const runId = core.getInput('runId');
-    const componentId = core.getInput('componentId');
-    const sequenceNo = core.getInput('statusSequenceNo');
-    const ghActionType = core.getInput('ghActionType');
+const isActionEnabled = true;
 
-    const url = `${baseURL}/action/run/status`;
-    const payload = {
-        componentId: componentId,
-        runId: runId,
-        sequenceNo: sequenceNo,
-        ghActionType: ghActionType
+if (isActionEnabled) {
+    try {
+
+        const baseURL = core.getInput('baseURL');
+        const runId = core.getInput('runId');
+        const componentId = core.getInput('componentId');
+        const sequenceNo = core.getInput('statusSequenceNo');
+        const ghActionType = core.getInput('ghActionType');
+
+        const url = `${baseURL}/action/run/status`;
+        const payload = {
+            componentId: componentId,
+            runId: runId,
+            sequenceNo: sequenceNo,
+            ghActionType: ghActionType
+        }
+
+        axios.post(url, payload).then(
+            () => {
+                core.setOutput("choreo-action-run-status", "saved");
+                console.log("choreo-action-run-status", "saved");
+            }
+        ).catch((error => {
+            if (error.response) {
+                core.setFailed(error.response.data);
+                console.log(error.response.status);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log('Error', error.message);
+                core.setOutput("choreo-status", "failed");
+                core.setFailed(error.message);
+            }
+        }))
+    } catch (e) {
+        core.setOutput("choreo-action-run-status-save", "failed");
+        core.setFailed(e.message);
+        console.log("choreo-action-run-status-save", "failed");
+        console.log(e.message);
     }
-
-    axios.post(url, payload).then(
-        () => {
-            core.setOutput("choreo-action-run-status", "saved");
-            console.log("choreo-action-run-status", "saved");
-        }
-    ).catch((error => {
-        if (error.response) {
-            core.setFailed(error.response.data);
-            console.log(error.response.status);
-        } else if (error.request) {
-            console.log(error.request);
-        } else {
-            console.log('Error', error.message);
-            core.setOutput("choreo-status", "failed");
-            core.setFailed(error.message);
-        }
-    }))
-} catch (e) {
-    core.setOutput("choreo-action-run-status-save", "failed");
-    core.setFailed(e.message);
-    console.log("choreo-action-run-status-save", "failed");
-    console.log(e.message);
+} else {
+    core.setOutput("choreo-action-run-status-save", "disabled");
+    console.log("choreo-action-run-status-save", "disabled");
 }
