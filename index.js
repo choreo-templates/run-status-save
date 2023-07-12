418e63aa7045ceb61c62ab97764f93cd7c21eba2
achala dias <achalad@wso2.com>
2023-07-12 23:51:06 +0530

    dep methods removed

--- index.js
+++ index.js
@@ -22,27 +22,23 @@ if (isActionEnabled) {
         console.log("Payload : ", payload);
         axios.post(url, payload).then(
             () => {
-                core.setOutput("choreo-action-run-status", "saved");
                 console.log("choreo-action-run-status", "saved");
             }
         ).catch((error => {
             console.error('Error', error);
             if (error.response) {
-                core.setOutput("choreo-status",error.response.data);
-                console.log(error.response.status);
+                console.log("choreo-status", error.response.status);
             } else if (error.request) {
                 console.log(error.request);
             } else {
                 console.log('Error', error.message);
-                core.setOutput("choreo-status", "failed");
+                console.log("choreo-status", "failed");
             }
         }))
     } catch (e) {
-        core.setOutput("choreo-action-run-status-save", "failed");
         console.log("choreo-action-run-status-save", "failed");
         console.log("choreo-action-run-status-save", e.message);
     }
 } else {
-    core.setOutput("choreo-action-run-status-save", "disabled");
     console.log("choreo-action-run-status-save", "disabled");
 }
